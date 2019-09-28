b13ace2de17bf78af5a8aca3285396a337d28fbd
nigmatullin-trpo <nigmatullin-trpo@mail.ru>
2019-09-28 20:48:55 +0700

    Implementing user input

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	float t = 0;
+    float s = 0;
+
+    printf("Enter deposit time: ");		scanf("%f", &t);
+    printf("Enter deposit amount: ");   scanf("%f", &s);
+
+	return 0;
+}
