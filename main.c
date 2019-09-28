1d70d87abcb491e4919ffda4c6d123ff52c42e8f
nigmatullin-trpo <nigmatullin-trpo@mail.ru>
2019-09-28 20:50:43 +0700

    Implementing input validation functions

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int InputTime(float t)
+{
+    if (t >= 0 && t <= 365)
+    return 1;
+    return 0;
+}
+ 
+int InputSum(float s)
+{
+    if (s >= 10000)
+    return 1;
+    return 0;
+}
+
 int main()
 {
 	float t = 0;
     float s = 0;
 
-    printf("Enter deposit time: ");		scanf("%f", &t);
-    printf("Enter deposit amount: ");   scanf("%f", &s);
+    while(1)
+    {
+        printf("Enter deposit time: ");
+        scanf("%f", &t);
+        if(InputTime(t)) break;
+        printf("Incorrect value, try again \n");
+    }
+ 
+    while(1)
+    {
+        printf("Enter deposit amount: ");
+        scanf("%f", &s);
+        if(InputSum(s)) break;
+        printf("Incorrect value, try again \n");
+    }
 
 	return 0;
 }
