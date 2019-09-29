954b1e285bc3e2ecc9e2af587b1b1781a47328a7
nigmatullin-trpo <nigmatullin-trpo@mail.ru>
2019-09-29 14:55:22 +0700

    File deposit.h has been updated

---
+++ src/deposit.h
@@ -0,0 +1,14 @@
+#ifndef TIME
+#define TIME
+int InputTime(float t);
+#endif
+
+#ifndef SUM
+#define SUM
+int InputSum(float s);
+#endif
+
+#ifndef CALC
+#define CALC
+float TaskCalculations(float t, float s);
+#endif
